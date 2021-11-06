9871d63e76cdfc0ef7e17b6ba3abc680c9e399a6
Julibonita <julii.r1809@hotmail.com>
2021-11-06 17:48:50 -0500

    Final

--- subasta.go
+++ subasta.go
@@ -6,33 +6,37 @@ func main() {
 
 	var name string
 	var preini int
-	var precio int
+	var precio_oferta int
+	var precio_actual int
 	var resp string
 	fmt.Println("Bienvenido a Subastas JyD. Por favor ingrese el nombre del producto a subastar")
 	fmt.Scanf("%s", &name)
 	fmt.Println("Por favor ingrese el Valor inicial del producto")
 	fmt.Scanf("%d", &preini)
-	fmt.Println("Inicia la subasta de", name, "por un valor de", preini)
 
-	fmt.Println("¿Alguien da más?")
-	fmt.Println("Digite la palabra si para continuar o la palabra no para terminar la subasta")
-	fmt.Scanf("%s", &resp)
+	precio_actual = preini
+	fmt.Println("Inicia la subasta de", name, "por un valor de", precio_actual)
 
-	switch resp {
-	case "si":
-		fmt.Println("Recuerde que el nuevo valor debe ser superior al valor inicial y al valor aceptado anteriormente.\nPor favor ingrese el nuevo valor")
-		fmt.Scanf("%d", &precio)
-		if precio > preini {
-			fmt.Println("Valor aceptado. Ahora", name, "se esta subastando por", precio)
-			precio = preini
+	for {
+		fmt.Println("El valor actual es:", precio_actual)
+		fmt.Println("¿Alguien da más?")
+
+		fmt.Println("Digite la palabra si para continuar o la palabra no para terminar la subasta")
+		fmt.Scanf("%s", &resp)
+
+		if resp == "si" {
+
+			fmt.Println("Recuerde que el nuevo valor debe ser superior al valor inicial y al valor aceptado anteriormente.\nPor favor ingrese el nuevo valor")
+			fmt.Scanf("%d", &precio_oferta)
+			if precio_oferta > precio_actual {
+				precio_actual = precio_oferta
+				fmt.Println("Valor aceptado. Ahora", name, "se esta subastando por", precio_actual)
+			} else {
+				fmt.Println("Este valor no puede ser aceptado ya que es menor al precio inicial y al valor aceptado anteriormente.")
+			}
 		} else {
-			fmt.Println("Este valor no puede ser aceptado ya que es menor al precio inicial y al valor aceptado anteriormente.")
+			fmt.Println("Subasta terminada.", name, "Fue vendido por un valor de", precio_actual)
+			break
 		}
-	case "no":
-		fmt.Println("Subasta terminada.", name, "Fue vendido por un valor de", preini)
-
-	default:
-		fmt.Println("Lo siento, no reconocimos su respuesta. Por favor intente de nuevo")
 	}
-
 }
